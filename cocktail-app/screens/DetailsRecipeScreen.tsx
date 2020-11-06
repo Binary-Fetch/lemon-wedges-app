cd8c5e8de107388f8fc71461e52af8f7a5dfe6de
Kushal Biswas <biswas.kushal@gmail.com>
2020-11-07 00:20:02 +0530

    details page

--- cocktail-app/screens/DetailsRecipeScreen.tsx
+++ cocktail-app/screens/DetailsRecipeScreen.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { StyleSheet, Image, SectionList, SafeAreaView } from 'react-native';
+import { StyleSheet, Image, SectionList, SafeAreaView, useColorScheme } from 'react-native';
 import RecipeItem from '../components/RecipeItem';
 import { View , Text} from '../components/Themed';
+import Colors from '../constants/Colors';
 import { DetailRecipeComponent, Ingredients } from '../types';
 
 export default class DetailRecipeScreen extends React.Component<DetailRecipeComponent.Props, DetailRecipeComponent.State> {
@@ -12,10 +13,8 @@ export default class DetailRecipeScreen extends React.Component<DetailRecipeComp
         
     }
 
-
-
-
     render() {
+       
         const { recipeDetails }= this.props.route.params;
         let IngeredientSectionList;
         if (Array.isArray(recipeDetails.ingredients)) {
@@ -27,18 +26,23 @@ export default class DetailRecipeScreen extends React.Component<DetailRecipeComp
             });
         }
         return (
-            <SafeAreaView style={styles.container}>
+            <SafeAreaView style={styles.containerroot}>
                 <Text style={styles.title}>{recipeDetails.name}</Text>
                 <Item imageUri={recipeDetails.imageUrl}  />
-                <Text>Ingredients</Text>
-                <SectionList<Ingredients>
+                <Text style={styles.title2}>Ingredients</Text>
+                <View style={styles.container}>
+                 <SectionList<Ingredients>
                 sections={IngeredientSectionList}
                 keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Text> {item.ingredient.name}-{item.amount}</Text>}
-                
+        renderItem={({ item }) => <View style={ styles.ingredientrow}><Text style={styles.ingredient}>{item.ingredient.name}</Text><Text style={styles.amount}>{item.amount}</Text></View>}  
             />
-            <Text>Prepare Steps</Text>
+            </View>
+            <Text style={styles.title2}>Prepare Steps</Text>
+            <View style={styles.container}>
+            
             <Text>{recipeDetails.prepareSteps[0].description}</Text> 
+            </View>
+            <Text></Text>
                {/* <RecipeItem recipeDetails={this.state.coctailRecipe}></RecipeItem> */}
             </SafeAreaView>
         );
@@ -54,7 +58,7 @@ const Item = ({ imageUri }: { imageUri: string[] | undefined }) => {
 
 }
 const styles = StyleSheet.create({
-    container: {
+    containerroot: {
         flex: 1,
         shadowColor: "#000",
         shadowOffset: {
@@ -67,18 +71,56 @@ const styles = StyleSheet.create({
         padding: 5,
         marginTop: 50,
         marginBottom: 5,
+        marginLeft: 20,
+        marginRight: 20,
+       
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        justifyContent: 'flex-start',
+        overflow: "scroll"
+    },container: {
+        
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 2,
+        padding: 20,
+        marginBottom: 5,
         marginLeft: 2,
         marginRight: 2,
-        borderRadius: 5
+        borderWidth: 0.5,
+        justifyContent: 'flex-start',
     },title: {
+        flex: 0.3,
+        
+        backgroundColor: "grey",
+        borderWidth: 0.5,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
         fontSize: 24,
+        textAlign: "center"
+    },title2: {
+        flex: 0.3,
+        marginTop: 5,
+        marginLeft: 2,
+        marginRight: 2,
+        backgroundColor: "grey",
+        borderWidth: 0.5,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        fontSize: 20,
         textAlign: "center"
     },image: {
         width: "100%",
-        height: 200,
+        height: 250,
         borderTopLeftRadius: 5,
         borderTopRightRadius: 5,
         overflow: "hidden",
+        resizeMode:'contain'
     },
     formLabel: {
         fontSize: 20
@@ -95,4 +137,25 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         backgroundColor: '#DCDCDC',
     },
+    ingredient: {
+        flex: 0.6,
+        //backgroundColor: "#c2d9ff",
+        borderColor:"#c2d9ff",
+        borderWidth:0.5,
+        //color: "red",
+        fontWeight:"500",
+        fontSize: 10,
+        textAlign: "center"
+    },amount: {
+        flex: 0.4,
+        borderColor: "#fcffde",
+        borderWidth:0.5,
+        fontSize: 10,
+        fontWeight:"500",
+        //color:"red",
+        textAlign: "center"
+    },ingredientrow:{
+        flexDirection: "row",
+        height: 'auto'
+    }
 });
