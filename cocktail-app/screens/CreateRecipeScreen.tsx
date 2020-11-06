150541f7d3ad062454b3974cb13eb65f7eaa5549
Ayan Dutta <ayan.dutta94@outlook.com>
2020-11-07 01:51:33 +0530

    Scroll Fix

--- cocktail-app/screens/CreateRecipeScreen.tsx
+++ cocktail-app/screens/CreateRecipeScreen.tsx
@@ -2,6 +2,7 @@ import { FieldArray, Formik } from 'formik';
 import * as React from 'react';
 import { Button, StyleSheet, TextInput } from 'react-native';
 import { Button as ThemedButton, ThemeProvider } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Yup from 'yup';
@@ -74,113 +75,115 @@ class CreateRecipeScreen extends React.Component<CreateRecipeComponent.Props, Cr
                     }}
                 >
                     {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-                        <View style={styles.container}>
-                            <View>
-                                <Text style={styles.formLabel}>Add Recipe</Text>
-                                <TextInput
-                                    placeholder="Name"
-                                    onChangeText={handleChange('name')}
-                                    onBlur={handleBlur('name')}
-                                    value={values.name}
-                                    style={styles.inputStyle}
-                                />
-                                {errors.name && touched.name ? (
-                                    <Text style={{ color: "#f00" }}>{errors.name}</Text>
-                                ) : null}
-                                <TextInput
-                                    placeholder="Description"
-                                    onChangeText={handleChange('desc')}
-                                    onBlur={handleBlur('desc')}
-                                    value={values.desc}
-                                    multiline={true}
-                                    style={styles.inputStyle}
-                                />
-                                {errors.desc && touched.desc ? (
-                                    <Text style={{ color: "#f00" }}>{errors.desc}</Text>
-                                ) : null}
-                                <TextInput
-                                    placeholder="Image Url"
-                                    onChangeText={handleChange('imageUrl')}
-                                    onBlur={handleBlur('imageUrl')}
-                                    value={values.imageUrl}
-                                    multiline={true}
-                                    style={styles.inputStyle}
-                                />
-                                {errors.imageUrl && touched.imageUrl ? (
-                                    <Text style={{ color: "#f00" }}>{errors.imageUrl}</Text>
-                                ) : null}
+                        <ScrollView>
+                            <View style={styles.container}>
+                                <View>
+                                    <Text style={styles.formLabel}>Add Recipe</Text>
+                                    <TextInput
+                                        placeholder="Name"
+                                        onChangeText={handleChange('name')}
+                                        onBlur={handleBlur('name')}
+                                        value={values.name}
+                                        style={styles.inputStyle}
+                                    />
+                                    {errors.name && touched.name ? (
+                                        <Text style={{ color: "#f00" }}>{errors.name}</Text>
+                                    ) : null}
+                                    <TextInput
+                                        placeholder="Description"
+                                        onChangeText={handleChange('desc')}
+                                        onBlur={handleBlur('desc')}
+                                        value={values.desc}
+                                        multiline={true}
+                                        style={styles.inputStyle}
+                                    />
+                                    {errors.desc && touched.desc ? (
+                                        <Text style={{ color: "#f00" }}>{errors.desc}</Text>
+                                    ) : null}
+                                    <TextInput
+                                        placeholder="Image Url"
+                                        onChangeText={handleChange('imageUrl')}
+                                        onBlur={handleBlur('imageUrl')}
+                                        value={values.imageUrl}
+                                        multiline={true}
+                                        style={styles.inputStyle}
+                                    />
+                                    {errors.imageUrl && touched.imageUrl ? (
+                                        <Text style={{ color: "#f00" }}>{errors.imageUrl}</Text>
+                                    ) : null}
+                                </View>
+                                <FieldArray name="ingredients">
+                                    {({ insert, remove, push }) => (
+                                        <View style={{ borderWidth: 0.5, padding: 5, marginTop: 5 }}>
+                                            <Text style={styles.headerContent}>Ingredients</Text>
+                                            {values.ingredients.length > 0 &&
+                                                values.ingredients.map((ingredient, index) => (
+                                                    <View key={index}>
+                                                        <TextInput
+                                                            placeholder="Name"
+                                                            onChangeText={handleChange(`ingredients[${index}].ingredient.name`)}
+                                                            onBlur={handleBlur(`ingredients[${index}].ingredient.name`)}
+                                                            value={ingredient.ingredient.name}
+                                                            style={styles.inputStyle}
+                                                        />
+                                                        <TextInput
+                                                            placeholder="Amount"
+                                                            onChangeText={handleChange(`ingredients[${index}].amount`)}
+                                                            onBlur={handleBlur(`ingredients[${index}].amount`)}
+                                                            value={ingredient.amount}
+                                                            style={styles.inputStyle}
+                                                        />
+                                                        <TextInput
+                                                            placeholder="Type"
+                                                            onChangeText={handleChange(`ingredients[${index}].type`)}
+                                                            onBlur={handleBlur(`ingredients[${index}].type`)}
+                                                            value={ingredient.type}
+                                                            style={styles.inputStyle}
+                                                        />
+                                                        <View style={styles.separator}>
+                                                            <Button title="Remove Ingredient" onPress={() => remove(index)} />
+                                                        </View>
+                                                    </View>
+                                                ))}
+                                            <View style={styles.separator}>
+                                                <Button title="Add Ingredient" onPress={() => push({ amount: '', type: '', ingredient: { name: '' } })} />
+                                            </View>
+                                        </View>
+                                    )}
+                                </FieldArray>
+                                <FieldArray name="prepareSteps">
+                                    {({ insert, remove, push }) => (
+                                        <View style={{ borderWidth: 0.5, padding: 5, marginTop: 5 }}>
+                                            <Text style={styles.headerContent}>Preparation Steps</Text>
+                                            {values.prepareSteps.length > 0 &&
+                                                values.prepareSteps.map((prepareStep, index) => (
+                                                    <View key={index}>
+                                                        <TextInput
+                                                            placeholder="Description"
+                                                            onChangeText={handleChange(`prepareSteps[${index}].description`)}
+                                                            onBlur={handleBlur(`prepareSteps[${index}].description`)}
+                                                            value={prepareStep.description}
+                                                            multiline={true}
+                                                            style={styles.inputStyle}
+                                                        />
+                                                        <View style={styles.separator}>
+                                                            <Button title="Remove Preparation Step" onPress={() => remove(index)} />
+                                                        </View>
+                                                    </View>
+                                                ))}
+                                            <View style={styles.separator}>
+                                                <Button title="Add Preperation Step" onPress={() => push({ description: '', order: 1 })} />
+                                            </View>
+                                        </View>
+                                    )}
+                                </FieldArray>
+                                <ThemedButton style={{ margin: 5 }} title={submissionInProg ? 'Adding...' : 'Submit'} disabled={submissionInProg} onPress={(e: any) => handleSubmit(e)} />
+                                {/* <Button onPress={handleSubmit} title="Submit" /> */}
+                                {creationMessage &&
+                                    <Text style={{ color: '#f00' }}>{creationMessage}</Text>
+                                }
                             </View>
-                            <FieldArray name="ingredients">
-                                {({ insert, remove, push }) => (
-                                    <View style={{ borderWidth: 0.5, padding: 5, marginTop: 5 }}>
-                                        <Text style={styles.headerContent}>Ingredients</Text>
-                                        {values.ingredients.length > 0 &&
-                                            values.ingredients.map((ingredient, index) => (
-                                                <View key={index}>
-                                                    <TextInput
-                                                        placeholder="Name"
-                                                        onChangeText={handleChange(`ingredients[${index}].ingredient.name`)}
-                                                        onBlur={handleBlur(`ingredients[${index}].ingredient.name`)}
-                                                        value={ingredient.ingredient.name}
-                                                        style={styles.inputStyle}
-                                                    />
-                                                    <TextInput
-                                                        placeholder="Amount"
-                                                        onChangeText={handleChange(`ingredients[${index}].amount`)}
-                                                        onBlur={handleBlur(`ingredients[${index}].amount`)}
-                                                        value={ingredient.amount}
-                                                        style={styles.inputStyle}
-                                                    />
-                                                    <TextInput
-                                                        placeholder="Type"
-                                                        onChangeText={handleChange(`ingredients[${index}].type`)}
-                                                        onBlur={handleBlur(`ingredients[${index}].type`)}
-                                                        value={ingredient.type}
-                                                        style={styles.inputStyle}
-                                                    />
-                                                    <View style={styles.separator}>
-                                                        <Button title="Remove Ingredient" onPress={() => remove(index)} />
-                                                    </View>
-                                                </View>
-                                            ))}
-                                        <View style={styles.separator}>
-                                            <Button title="Add Ingredient" onPress={() => push({ amount: '', type: '', ingredient: { name: '' } })} />
-                                        </View>
-                                    </View>
-                                )}
-                            </FieldArray>
-                            <FieldArray name="prepareSteps">
-                                {({ insert, remove, push }) => (
-                                    <View style={{ borderWidth: 0.5, padding: 5, marginTop: 5 }}>
-                                        <Text style={styles.headerContent}>Preparation Steps</Text>
-                                        {values.prepareSteps.length > 0 &&
-                                            values.prepareSteps.map((prepareStep, index) => (
-                                                <View key={index}>
-                                                    <TextInput
-                                                        placeholder="Description"
-                                                        onChangeText={handleChange(`prepareSteps[${index}].description`)}
-                                                        onBlur={handleBlur(`prepareSteps[${index}].description`)}
-                                                        value={prepareStep.description}
-                                                        multiline={true}
-                                                        style={styles.inputStyle}
-                                                    />
-                                                    <View style={styles.separator}>
-                                                        <Button title="Remove Preparation Step" onPress={() => remove(index)} />
-                                                    </View>
-                                                </View>
-                                            ))}
-                                        <View style={styles.separator}>
-                                            <Button title="Add Preperation Step" onPress={() => push({ description: '', order: 1 })} />
-                                        </View>
-                                    </View>
-                                )}
-                            </FieldArray>
-                            <ThemedButton style={{ margin: 5 }} title={submissionInProg ? 'Adding...' : 'Submit'} disabled={submissionInProg} onPress={(e: any) => handleSubmit(e)} />
-                            {/* <Button onPress={handleSubmit} title="Submit" /> */}
-                            {creationMessage &&
-                                <Text style={{ color: '#f00' }}>{creationMessage}</Text>
-                            }
-                        </View>
+                        </ScrollView>
                     )}
                 </Formik>
             </ThemeProvider>
@@ -206,7 +209,6 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         width: "98%",
-        overflow: "scroll",
         justifyContent: 'flex-start',
     },
     formLabel: {
